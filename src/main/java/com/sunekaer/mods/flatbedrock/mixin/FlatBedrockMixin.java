d8fffa4c56d4376723550ac558f9fd68f3895362
Sunekaer <kaspar@sunekaer.com>
2020-07-28 22:13:12 +0200

    1.16

--- src/main/java/com/sunekaer/mods/flatbedrock/mixin/FlatBedrockMixin.java
+++ src/main/java/com/sunekaer/mods/flatbedrock/mixin/FlatBedrockMixin.java
@@ -7,9 +7,14 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(NoiseChunkGenerator.class)
 public abstract class FlatBedrockMixin {
-    @ModifyConstant(method = "makeBedrock", constant = {@Constant(intValue = 4), @Constant(intValue = -4)})
+    @ModifyConstant(method = "makeBedrock", constant = {@Constant(intValue = 4)})
     private int setBedrockHeight(int value){
         return 0;
+    }
+
+    @ModifyConstant(method = "makeBedrock", constant = {@Constant(intValue = 5)})
+    private int setBedrockHeight2(int value){
+        return 1;
     }
 }
 

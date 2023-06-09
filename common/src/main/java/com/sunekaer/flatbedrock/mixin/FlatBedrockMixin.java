c87901440ae3d1dd9cd752ebd8cf14a1f13feb8f
Sunekaer <git@sunekaer.com>
2023-06-09 20:38:38 +0200

    feat: port to 1.20

--- common/src/main/java/com/sunekaer/flatbedrock/mixin/FlatBedrockMixin.java
+++ common/src/main/java/com/sunekaer/flatbedrock/mixin/FlatBedrockMixin.java
@@ -1,53 +1,35 @@
 package com.sunekaer.flatbedrock.mixin;
 
-import net.minecraft.data.worldgen.SurfaceRuleData;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SurfaceRuleData.class)
-public abstract class FlatBedrockMixin {
+@Mixin(targets = "net/minecraft/world/level/levelgen/SurfaceRules$VerticalGradientConditionSource")
+public class FlatBedrockMixin {
 
-    @ModifyArg(
-            method = "overworldLike",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/levelgen/VerticalAnchor;aboveBottom(I)Lnet/minecraft/world/level/levelgen/VerticalAnchor;"
-            ),
-            index = 0,
-            slice = @Slice(from = @At(value="CONSTANT", args = "stringValue=bedrock_floor"))
-    )
-    private static int aboveBottom(int i) {
-        return 1;
+    @Shadow @Final private ResourceLocation randomName;
+    private static final ResourceLocation bedrockFloor = new ResourceLocation("bedrock_floor");
+    private static final ResourceLocation bedrockRoof = new ResourceLocation("bedrock_roof");
+    private static final VerticalAnchor aboveBottom = VerticalAnchor.aboveBottom(1);
+    private static final VerticalAnchor belowTop = VerticalAnchor.belowTop(1);
+
+    @Inject(method = "falseAtAndAbove", at = @At("HEAD"), cancellable = true)
+    private void falseAtAndAbove(CallbackInfoReturnable<VerticalAnchor> cir) {
+        if(randomName.equals(bedrockFloor)) {
+            cir.setReturnValue(aboveBottom);
+        }
     }
 
-    @ModifyArg(
-            method = "nether",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/levelgen/VerticalAnchor;aboveBottom(I)Lnet/minecraft/world/level/levelgen/VerticalAnchor;",
-                    ordinal = 0
-            ),
-            index = 0,
-            slice = @Slice(from = @At(value="CONSTANT", args = "stringValue=bedrock_floor"))
-    )
-    private static int aboveBottomFloorNether(int i) {
-        return 1;
-    }
-
-    @ModifyArg(
-            method = "nether",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/levelgen/VerticalAnchor;belowTop(I)Lnet/minecraft/world/level/levelgen/VerticalAnchor;",
-                    ordinal = 0
-            ),
-            index = 0,
-            slice = @Slice(from = @At(value="CONSTANT", args = "stringValue=bedrock_roof"))
-    )
-    private static int belowTopRoofNether(int i) {
-        return 1;
+    @Inject(method = "trueAtAndBelow", at = @At("HEAD"), cancellable = true)
+    private void trueAtAndBelow(CallbackInfoReturnable<VerticalAnchor> cir) {
+        if(randomName.equals(bedrockRoof)) {
+            cir.setReturnValue(belowTop);
+        }
     }
 }
 
